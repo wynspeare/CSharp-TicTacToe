9dcfcc86991c221d1019f2a622ba76542d1147ca
wynspeare <caroline@8thlight.com>
2019-05-29 12:47:55 -0500

    .sln file setup and splitting into two projects - logic and tests

---
+++ TTTTests/TicTacToeTest.cs
@@ -0,0 +1,50 @@
+﻿using Xunit;
+using System;
+using TicTacToeApp;
+
+namespace TicTacToeTests
+{
+    public class TTTTest
+    {
+        [Fact]
+        public void userCanStartNewGame()
+        {
+            TicTacToe myTTT = new TicTacToe();
+            Assert.Equal("A new game has been started", myTTT.Main());
+        }
+
+        // [Fact]
+        // public void playerOneCanChooseAMarker()
+        // {
+        //     TicTacToe myTTT = new TicTacToe();
+        //     myTTT.chooseMarker();
+        //     Assert.NotNull(myTTT.playerOneMarker);
+        //     Assert.NotNull(myTTT.playerTwoMarker);
+        // }
+
+        // [Fact]
+        // public void userCanReadInstructions()
+        // {
+        //     TicTacToe myTTT = new TicTacToe();
+        //     Assert.Contains("Players alternate placing", myTTT.displayInstructions());
+        // }
+
+
+        // [Fact]
+        // public void aNewGameHasAnEmptyBoard()
+        // {
+        //     TicTacToe myTTT = new TicTacToe();
+        //     myTTT.startNewGame();
+        //     Assert.True(myTTT.currentBoard.isEmpty());
+        // }
+
+        // [Fact]
+        // public void userCanViewTheBoard()
+        // {
+        //     TicTacToe myTTT = new TicTacToe();
+        //     myTTT.startNewGame();
+        //     Assert.Equal( " _ _ _ \n _ _ _ \n _ _ _ " , myTTT.currentBoard.displayBoard());
+        // }
+
+    }
+}
