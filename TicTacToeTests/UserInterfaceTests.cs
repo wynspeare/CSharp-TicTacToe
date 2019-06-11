f12cdfd81a9f85ff895d824703918dda089e09d3
wynspeare <caroline@8thlight.com>
2019-06-11 13:31:20 -0500

    Modified UI Tests

--- TicTacToeTests/UserInterfaceTests.cs
+++ TicTacToeTests/UserInterfaceTests.cs
@@ -11,42 +11,64 @@ namespace TicTacToeTests
 
         public const string EMPTY = "_";
         public const string P1_MARKER = "+";
-        public const string P2_MARKER = "*";
+        public const string P2_MARKER = "o";
+
 
         [Trait("Category", "UITest")]
         [Fact]
-        public void userIntefaceCanStartNewGameWithUserGivenSymbols()
+        public void markersCanBeSetWithUserProvidedSymbols()
         {
             var subject = new UserInterface();
-            subject.startNewGame();
+            var options = new Options(subject.setMarkers());
 
-            subject.newGame.moveMarker(3, subject.newGame.playerOne.marker);
-            subject.newGame.moveMarker(6, subject.newGame.playerTwo.marker);
-
-            subject.displayBoard(subject.newGame.currentBoard);
-
-            Assert.Equal(1, subject.newGame.currentBoard.board[0].location);
+            Assert.Equal(typeof(string), options.P1_MARKER.GetType());
         }
+
+        [Trait("Category", "UITest")]
+        [Fact]
+        public void markersCanBeSetWithGivenSymbols()
+        {
+            var subject = new UserInterface();
+            var markers = new Tuple<string, string> ( P1_MARKER, P2_MARKER );
+            var options = new Options(markers);
+
+            Assert.Equal("+", options.P1_MARKER);
+            Assert.Equal("o", options.P2_MARKER);
+        }
+
+
+        [Trait("Category", "UITest")]
+        [Fact]
+        public void markersCannotBeTheSame()
+        {
+            var subject = new UserInterface();
+
+            Assert.True(subject.isMarkerDifferent(P1_MARKER, P2_MARKER));
+            Assert.False(subject.isMarkerDifferent(P1_MARKER, P1_MARKER));
+        }
+
 
         [Trait("Category", "UITest")]
         [Fact]
         public void userCanKnowIfSelectedSpaceIsValid()
         {
             var subject = new UserInterface();
-            subject.startNewGame();
-            Assert.True(subject.isValidSpace("9"));   
-            Assert.False(subject.isValidSpace("-1"));
-            Assert.False(subject.isValidSpace("11"));
-            Assert.False(subject.isValidSpace("Q"));   
+            var myBoard = new Board();
+
+            Assert.True(subject.isValidSpace("9", myBoard.board));   
+            Assert.False(subject.isValidSpace("-1", myBoard.board));
+            Assert.False(subject.isValidSpace("11", myBoard.board));
+            Assert.False(subject.isValidSpace("Q", myBoard.board));   
         }
+
 
         [Trait("Category", "UITest")]
         [Fact]
         public void userCanEnterASpaceAndItsReturnsAnInteger()
         {
             var subject = new UserInterface();
-            subject.startNewGame();
-            Assert.Equal(typeof(int), subject.getValidSpace().GetType());
+            var myBoard = new Board();
+            Assert.Equal(typeof(int), subject.getValidSpace(myBoard.board, P1_MARKER).GetType());
         }
 
         [Trait("Category", "UITest")]
